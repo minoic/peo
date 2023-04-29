322c10a8fa1a29105fcf0215563465e57291d9ed
minoic <minoic2020@gmail.com>
2023-04-29 18:42:01 +0800

    fix: tr function

--- internal/controllers/i18n.go
+++ internal/controllers/i18n.go
@@ -1,5 +1,10 @@
 package controllers
 
+import (
+	"github.com/beego/i18n"
+	"github.com/minoic/peo/internal/configure"
+)
+
 func tr(input string) string {
-	return tr(input)
+	return i18n.Tr(configure.Viper().GetString("Language"), input)
 }
